8eeb647b63ab44a416297dc9750896a65a7fc5c6
kaliganoff <kaliganoff@yahoo.com>
2023-10-20 17:02:34 +0300

    feat: Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let config of bracketsConfig) {
+      if (str[i] === config[0]) {
+        if (config[0] === config[1]) {
+          if (stack.length === 0 || config[0] !== stack[stack.length - 1]) {
+            stack.push(str[i]);
+          } else {
+            stack.pop();
+          }
+        } else {
+          stack.push(str[i]);
+        }
+      } else if (str[i] === config[1]) {
+        if (stack.length === 0 || config[0] !== stack.pop()) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
